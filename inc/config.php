174c1c3a47a127e452da52dae0b0b6fd2fe0ca1a
Liut <liutao@liut.cc>
2015-06-04 14:45:49 +0800

    change default constants

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?PHP
 
 define('LDAP_HOST', 'slapd');
-define('LDAP_PORT', 10389);
+define('LDAP_PORT', 389);
 define('LDAP_BASE_DN', 'dc=example,dc=org');
 
 define('LDAP_PASSWORD_HASH', 'blowfish');
