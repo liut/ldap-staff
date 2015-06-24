b9f57f3c29af607032094a9c80a96db3ecee9fc7
Liut <liutao@liut.cc>
2015-06-24 12:53:54 +0800

    remove Horde cipher library;

--- class/Blowfish.php
+++ class/Blowfish.php
@@ -19,37 +19,29 @@ class Blowfish
 	 */
 	public static function encrypt($data, $secret=null) {
 
-	    # If our secret is null or blank, get the default.
-	    if ($secret === null || ! trim($secret))
-	        $secret = isset($_SESSION['BLOWFISH_SALT'])? $_SESSION['BLOWFISH_SALT'] : session_id();
+		# If our secret is null or blank, get the default.
+		if ($secret === null || ! trim($secret))
+			$secret = isset($_SESSION['BLOWFISH_SALT'])? $_SESSION['BLOWFISH_SALT'] : session_id();
 
-	    # If the secret isnt set, then just return the data.
-	    if (! trim($secret))
-	        return $data;
+		# If the secret isnt set, then just return the data.
+		if (! trim($secret))
+			return $data;
 
-	    if (function_exists('mcrypt_module_open') && ! empty($data)) {
-	        $td = mcrypt_module_open(MCRYPT_BLOWFISH,'',MCRYPT_MODE_ECB,'');
-	        $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td),MCRYPT_DEV_URANDOM);
-	        mcrypt_generic_init($td,substr($secret,0,mcrypt_enc_get_key_size($td)),$iv);
-	        $encrypted_data = base64_encode(mcrypt_generic($td,$data));
-	        mcrypt_generic_deinit($td);
+		if (!function_exists('mcrypt_module_open')) {
+			throw new Exception('need mcrypt extension');
+		}
 
-	        return $encrypted_data;
-	    }
+		if (empty($data)) {
+			return $data;
+		}
 
-	    $pma_cipher = new Horde_Cipher_Blowfish;
-	    $encrypt = '';
+		$td = mcrypt_module_open(MCRYPT_BLOWFISH,'',MCRYPT_MODE_ECB,'');
+		$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td),MCRYPT_DEV_URANDOM);
+		mcrypt_generic_init($td,substr($secret,0,mcrypt_enc_get_key_size($td)),$iv);
+		$encrypted_data = base64_encode(mcrypt_generic($td,$data));
+		mcrypt_generic_deinit($td);
 
-	    for ($i=0; $i<strlen($data); $i+=8) {
-	        $block = substr($data, $i, 8);
-
-	        if (strlen($block) < 8)
-	            $block = full_str_pad($block,8,"\0", 1);
-
-	        $encrypt .= $pma_cipher->encryptBlock($block, $secret);
-	    }
-
-	    return base64_encode($encrypt);
+		return $encrypted_data;
 	}
 
 	/**
@@ -62,41 +54,35 @@ class Blowfish
 	 */
 	public static function decrypt($encdata, $secret=null) {
 
-	    # This cache gives major speed up for stupid callers :)
-	    static $CACHE = array();
+		# This cache gives major speed up for stupid callers :)
+		static $CACHE = array();
 
-	    if (isset($CACHE[$encdata]))
-	        return $CACHE[$encdata];
+		if (isset($CACHE[$encdata]))
+			return $CACHE[$encdata];
 
-	    # If our secret is null or blank, get the default.
-	    if ($secret === null || ! trim($secret))
-	        $secret = isset($_SESSION['BLOWFISH_SALT'])? $_SESSION['BLOWFISH_SALT'] : session_id();
+		# If our secret is null or blank, get the default.
+		if ($secret === null || ! trim($secret))
+			$secret = isset($_SESSION['BLOWFISH_SALT'])? $_SESSION['BLOWFISH_SALT'] : session_id();
 
-	    # If the secret isnt set, then just return the data.
-	    if (! trim($secret))
-	        return $encdata;
+		# If the secret isnt set, then just return the data.
+		if (! trim($secret))
+			return $encdata;
 
-	    if (function_exists('mcrypt_module_open') && ! empty($encdata)) {
-	        $td = mcrypt_module_open(MCRYPT_BLOWFISH,'',MCRYPT_MODE_ECB,'');
-	        $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td),MCRYPT_DEV_URANDOM);
-	        mcrypt_generic_init($td,substr($secret,0,mcrypt_enc_get_key_size($td)),$iv);
-	        $decrypted_data = trim(mdecrypt_generic($td,base64_decode($encdata)));
-	        mcrypt_generic_deinit($td);
+		if (!function_exists('mcrypt_module_open')) {
+			throw new Exception('need mcrypt extension');
+		}
 
-	        return $decrypted_data;
-	    }
+		if (empty($encdata)) {
+			return $encdata;
+		}
 
-	    $pma_cipher = new Horde_Cipher_Blowfish;
-	    $decrypt = '';
-	    $data = base64_decode($encdata);
+		$td = mcrypt_module_open(MCRYPT_BLOWFISH,'',MCRYPT_MODE_ECB,'');
+		$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td),MCRYPT_DEV_URANDOM);
+		mcrypt_generic_init($td,substr($secret,0,mcrypt_enc_get_key_size($td)),$iv);
+		$decrypted_data = trim(mdecrypt_generic($td,base64_decode($encdata)));
+		mcrypt_generic_deinit($td);
 
-	    for ($i=0; $i<strlen($data); $i+=8)
-	        $decrypt .= $pma_cipher->decryptBlock(substr($data, $i, 8), $secret);
-
-	    // Strip off our \0's that were added.
-	    $return = preg_replace("/\\0*$/",'',$decrypt);
-	    $CACHE[$encdata] = $return;
-	    return $return;
+		return $decrypted_data;
 	}
 
 } // END class Blowfish
